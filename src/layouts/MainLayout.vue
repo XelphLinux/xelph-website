9695c9d0b8d32d4589ede4dcd8de9481b20881a2
leen-neel <and24903@gmail.com>
2021-10-18 16:36:07 +0530

    fixed color of toggle switch

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,11 +2,14 @@
   <q-layout view="lHh Lpr lFf">
     <q-header>
       <q-toolbar>
-        <q-toolbar-title> <strong>Xelph Linux</strong> </q-toolbar-title>
+        <q-toolbar-title class="title-header">
+          <strong>Xelph Linux</strong>
+        </q-toolbar-title>
         <q-btn
           flat
           round
           dense
+          :color="$q.dark.isActive ? 'light' : 'dark'"
           :icon="$q.dark.isActive ? 'light_mode' : 'dark_mode'"
           @click="$q.dark.toggle()"
         />
