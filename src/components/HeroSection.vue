da1f5e252935c2663cc7fc165b698c7279793875
leen-neel <and24903@gmail.com>
2021-10-18 15:58:01 +0530

    hero is a different section now

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,5 @@
+<template>
+  <q-parallax src="https://cdn.quasar.dev/img/parallax2.jpg" :speed="0.5">
+    <h1 class="text-bold text-center">Xelph Linux</h1>
+  </q-parallax>
+</template>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,17 @@
 <template>
   <q-page class="">
-    <q-parallax src="https://cdn.quasar.dev/img/parallax2.jpg" :speed="0.5">
-      <h1 class="text-bold text-center">Xelph Linux</h1>
-    </q-parallax>
+    <HeroSection />
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
+import HeroSection from "src/components/HeroSection.vue";
 export default defineComponent({
   name: "PageIndex",
+  components: {
+    HeroSection,
+  },
 });
 </script>
