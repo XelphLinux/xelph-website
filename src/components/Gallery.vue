71258e0eeb0e51a616adab33d4082b0d55fce3f2
leen-neel <and24903@gmail.com>
2021-10-18 16:24:34 +0530

    added carousel

---
+++ src/components/Gallery.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="content-section">
+    <div class="text-h3 q-pa-md text-bold">Gallery</div>
+
+    <q-carousel
+      swipeable
+      animated
+      arrows
+      autoplay
+      navigation
+      class="q-mt-sm"
+      v-model="slide"
+      transition-prev="scale"
+      transition-next="scale"
+      infinite
+    >
+      <q-carousel-slide
+        :name="1"
+        img-src="https://cdn.quasar.dev/img/mountains.jpg"
+      />
+      <q-carousel-slide
+        :name="2"
+        img-src="https://cdn.quasar.dev/img/parallax1.jpg"
+      />
+      <q-carousel-slide
+        :name="3"
+        img-src="https://cdn.quasar.dev/img/parallax2.jpg"
+      />
+      <q-carousel-slide
+        :name="4"
+        img-src="https://cdn.quasar.dev/img/quasar.jpg"
+      />
+    </q-carousel>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    return {
+      slide: ref(1),
+      fullscreen: ref(false),
+    };
+  },
+};
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,6 +3,8 @@
     <HeroSection />
 
     <AboutSection class="q-mt-xl" />
+
+    <Gallery class="q-mt-xl" />
   </q-page>
 </template>
 
@@ -11,11 +13,14 @@ import { defineComponent } from "vue";
 
 import HeroSection from "src/components/HeroSection.vue";
 import AboutSection from "src/components/AboutSection.vue";
+import Gallery from "src/components/Gallery.vue";
+
 export default defineComponent({
   name: "PageIndex",
   components: {
     HeroSection,
     AboutSection,
+    Gallery,
   },
 });
 </script>
