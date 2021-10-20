f0458c3d54a8e12d1099d03c576deab32218a323
Idlidev <idliyout@gmail.com>
2021-10-20 18:08:25 +0530

    centered gallery title, removed bold from title in first page

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,7 +1,7 @@
 <template>
   <q-parallax src="images/wall.jpg" :speed="0.3">
     <transition appear enter-active-class="animated fadeInDown slower">
-      <h1 class="text-bold text-center hero-text">Xelph Linux</h1>
+      <h1 class="text-center hero-text">Xelph Linux</h1>
     </transition>
   </q-parallax>
 </template>
