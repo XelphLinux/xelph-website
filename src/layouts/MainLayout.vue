63b901bff6a235332c82fdf50f3cce4cd2e7b25d
Shinyzenith <aakashsensharma@gmail.com>
2021-10-30 23:06:39 +0530

    reversed order

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,17 +5,17 @@
         <q-toolbar-title>
           <strong
             ><strong class="title-header"
-              >Website developed by
-              <a target="_blank" href="https://and249.netlify.app/"
-                >Anindo Neel Dutta</a
+              >OS developed by
+              <a target="_blank" href="https://github.com/shinyzenith/"
+                >Aakash Sen Sharma</a
               ></strong
             ></strong
           >
           <strong
             ><strong class="title-header"
-              > | OS developed by
-              <a target="_blank" href="https://github.com/shinyzenith/"
-                >Aakash Sen Sharma</a
+              > | Website developed by
+              <a target="_blank" href="https://and249.netlify.app/"
+                >Anindo Neel Dutta</a
               ></strong
             ></strong
           >
